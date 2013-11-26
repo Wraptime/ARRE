93bee2220161e6bf86a04a66662efef933722f9f
Freeman Latif <freemanon@gmail.com>
2013-11-26 15:18:23 +0000

    Initial Commit

---
+++ Assets/CameraMovementScript.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+var speed = 10.0;
+function Start () {
+
+}
+
+function Update () {
+	var dir : Vector3 = Vector3.zero;
+
+	// we assume that the device is held parallel to the ground
+	// and the Home button is in the right hand
+
+	// remap the device acceleration axis to game coordinates:
+	//  1) XY plane of the device is mapped onto XZ plane
+	//  2) rotated 90 degrees around Y axis
+	Debug.Log("x : " + Input.acceleration.x + " y : " + Input.acceleration.y + " z : " + Input.acceleration.z);
+	
+	//dir.x = -Input.acceleration.y;
+	//dir.x = Input.acceleration.x;
+	dir.y = Input.acceleration.z;
+
+	// clamp acceleration vector to the unit sphere
+	if (dir.sqrMagnitude > 1)
+		dir.Normalize();
+
+	// Make it move 10 meters per second instead of 10 meters per frame...
+	dir *= Time.deltaTime;
+
+	// Move object
+	transform.Translate (dir * speed);
+}
---
+++ Assets/MonkeyBehaviour.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var colorStart : Color = Color.red;
+var colorEnd : Color = Color.green;
+var duration : float = 1.0;
+	
+function Update () {
+	var rotation : float = Time.deltaTime * 100 * Mathf.Sin(Time.fixedTime * 10);
+	Debug.Log("" + rotation);
+	transform.Rotate(Vector3.forward *  rotation);	
+	//var color : Color = Color( Mathf.Sin(Time.deltaTime * 100), Mathf.Cos(Time.deltaTime * 100), Mathf.Sin(Time.deltaTime * 100));
+	//renderer.material.color = Color.white;
+	var lerp : float = Mathf.PingPong (Time.time, duration) / duration;
+	renderer.material.color = Color.Lerp (colorStart, colorEnd, lerp);
+}
